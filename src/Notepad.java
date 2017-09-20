4a2a908ab9be954b0a7ec39d77745992031f155b
feedows <eshyo@live.jp>
2017-09-20 16:01:51 +0800

    逻辑设计中

--- src/Notepad.java
+++ src/Notepad.java
@@ -4,18 +4,18 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
 
-public abstract class Notepad {
+public abstract class Notepad{
     public JFrame window ;
     public JTextArea textArea;
     public JScrollPane scrollRight;
     public JMenuBar menuBar;
     public JMenu file,edit,view;
     public File currentFile = null;
-    public boolean saveFlag = true;
+    public boolean saveFlag = false;
 
     public abstract void init();
     public abstract  void actionPerformed(ActionEvent e);
     public abstract void saveFile(File file);
-    public abstract void openFIle(File file);
+    public abstract void openFile(File file);
     public abstract int exitAsk();
 }
--- src/TextEditor.java
+++ src/TextEditor.java
@@ -3,11 +3,13 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 public class TextEditor extends Notepad {
 
     JMenuItem item;
+    int exitFlag = -1;
 
     public TextEditor(){
         init();
@@ -18,7 +20,8 @@ public class TextEditor extends Notepad {
     public void init() {
 
         window = new JFrame("Easy NotePad Demo beta1.0");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        //window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         menuBar = new JMenuBar();
         textArea = new JTextArea(30,70);
         scrollRight = new JScrollPane(textArea);
@@ -39,6 +42,7 @@ public class TextEditor extends Notepad {
                     }
                 }
         );
+
     }
 
     private void setUI(){
@@ -46,19 +50,30 @@ public class TextEditor extends Notepad {
         menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
         menuBar.add(file);
         file.add(item = new JMenuItem("新建"));
+        item.addActionListener(this::actionPerformed);
         file.add(item = new JMenuItem("打开"));
+        item.addActionListener(this::actionPerformed);
+        file.addSeparator();
         file.add(item = new JMenuItem("保存"));
+        item.addActionListener(this::actionPerformed);
         file.add(item = new JMenuItem("另存为"));
+        item.addActionListener(this::actionPerformed);
+        file.addSeparator();
         file.add(item = new JMenuItem("退出"));
+        item.addActionListener(this::actionPerformed);
 
         menuBar.add(edit);
         edit.add(item = new JMenuItem("查找"));
+        item.addActionListener(this::actionPerformed);
         edit.add(item = new JMenuItem("替换"));
+        item.addActionListener(this::actionPerformed);
 
 
         menuBar.add(view);
         view.add(item = new JMenuItem("字体"));
+        item.addActionListener(this::actionPerformed);
         view.add(item = new JMenuItem("颜色"));
+        item.addActionListener(this::actionPerformed);
 
 
         window.setLayout(new BorderLayout(2,1));
@@ -71,7 +86,23 @@ public class TextEditor extends Notepad {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        String command = e.getActionCommand();
+        if(command.equals("新建")){
+            System.out.println(command);
+        }
+        if(command.equals("打开")){
+            System.out.println(command);
+        }
+        if(command.equals("保存")){
+            System.out.println(command);
+        }
+        if(command.equals("退出")){
+            if(exitAsk() == 1){
+                System.exit(0);
+            }else{
+                return;
+            }
+        }
     }
 
     @Override
@@ -80,12 +111,48 @@ public class TextEditor extends Notepad {
     }
 
     @Override
-    public void openFIle(File file) {
+    public void openFile(File file) {
 
     }
 
-    @Override
+
     public int exitAsk() {
-        return 0;
+        if (saveFlag) {
+            exitFlag = 1;
+        } else {
+            final JDialog exitDialog = new JDialog(window, "注意");
+            exitDialog.add(new JLabel("该文本未被保存，是否保存？"), BorderLayout.CENTER);
+            JPanel buttonPanel = new JPanel(new FlowLayout());
+            JButton yes = new JButton("是");
+            JButton no = new JButton("否");
+
+            buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+            buttonPanel.add(yes);
+            buttonPanel.add(no);
+
+            exitDialog.add(buttonPanel,BorderLayout.SOUTH);
+            exitDialog.setLocationRelativeTo(window);
+            exitDialog.pack();
+            exitDialog.setVisible(true);
+
+            yes.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    saveFile(currentFile);
+                    saveFlag = true;
+                    exitFlag = 1;
+                }
+            });
+
+            no.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
+                }
+            });
+        }
+
+        return exitFlag;
     }
+
 }
